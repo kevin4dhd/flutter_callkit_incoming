999582178cdcc9c176176747542fdfbf3dbb9e8b
Kevin Piazzoli Felix Perez <kevin3dhd@gmail.com>
2024-10-18 19:39:09 -0500

    fix android v2

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
@@ -8,6 +8,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.SharedPreferences
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
